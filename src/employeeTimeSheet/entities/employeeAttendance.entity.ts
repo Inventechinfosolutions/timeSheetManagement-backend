754cbd147b2f14b36dc4fac606df640f6ba38355
arunkumar707 <arunkumar.a.n707@gmail.com>
2026-01-12 10:24:42 +0530

    Backend_EmployeeAttendence

--- src/employeeTimeSheet/entities/employeeAttendance.entity.ts
+++ src/employeeTimeSheet/entities/employeeAttendance.entity.ts
@@ -1,17 +1,14 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-export enum OfficeLocation {
-  OFFICE = 'Office',
-  WORK_FROM_HOME = 'Work from Home',
-  CLIENT_PLACE = 'Client Place',
-}
+
 
 export enum AttendanceStatus {
   FULL_DAY = 'Full Day',
   HALF_DAY = 'Half Day',
   LEAVE = 'Leave',
   PENDING = 'Pending',
-  NOT_UPDATED = 'Not Updated'
+  NOT_UPDATED = 'Not Updated',
+  WEEKEND = 'Weekend'
 }
 
 @Entity('employee_attendance')
@@ -25,22 +22,10 @@ export class EmployeeAttendance {
   @Column({ name: 'working_date', type: 'date' })
   workingDate: Date;
 
-  @Column({ name: 'login_time', nullable: true })
-  loginTime: string;
 
-  @Column({ name: 'logout_time', nullable: true })
-  logoutTime: string;
-
-  @Column({
-    name: 'location',
-    type: 'enum',
-    enum: OfficeLocation,
-    nullable: true,
-  })
-  location: string;
 
   @Column({ name: 'total_hours', type: 'float', nullable: true })
-  totalHours: number;
+  totalHours: number | null;
 
   @Column({
     name: 'status',
@@ -48,5 +33,5 @@ export class EmployeeAttendance {
     enum: AttendanceStatus,
     nullable: true,
   })
-  status: string;
+  status: AttendanceStatus | string | null;
 }
