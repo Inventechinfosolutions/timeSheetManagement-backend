4ce943de56062f37350a5491da7fcb7470497194
Kusuma H K <kusuma.hk@inventechinfo.com>
2026-07-23 15:37:38 +0530

    DB_script

---
+++ scripts/db.sql
@@ -0,0 +1,24 @@
+-- -- --------------------------
+-- -1.quarterly_reviews
+-- ----------------------------------
+CREATE TABLE `quarterly_reviews` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `employee_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quarter` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Draft','Submitted') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Draft',
+  `overview` text COLLATE utf8mb4_unicode_ci,
+  `achievements` json DEFAULT NULL,
+  `challenges` json DEFAULT NULL,
+  `learning_goals` json DEFAULT NULL,
+  `submitted_date` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdBy` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `updatedBy` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `manager_name` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ---------------------------------------------------
+-- 2.
+-- --------------------------------------------------------
