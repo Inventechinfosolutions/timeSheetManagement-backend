c039548283a34ca1dd5dff6a41af64fad78ee180
Udayraj2710 <udayraj.kb@inventechinfo.com>
2026-08-31 12:39:33 +0530

    PR raised for Appraisal – Manager side

--- src/appraisal/appraisal.module.ts
+++ src/appraisal/appraisal.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuarterlyReview } from './quarterlyReview/entities/quarterly-review.entity';
 import { QuarterlyReviewController } from './quarterlyReview/controllers/quarterly-review.controller';
 import { QuarterlyReviewService } from './quarterlyReview/services/quarterly-review.service';
+import { ManagerQuarterlyReviewModule } from './ManagerQuaterlyReview/manager-quarterly-review.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([QuarterlyReview])],
+  imports: [
+    TypeOrmModule.forFeature([QuarterlyReview]),
+    ManagerQuarterlyReviewModule,
+  ],
   controllers: [QuarterlyReviewController],
   providers: [QuarterlyReviewService],
   exports: [QuarterlyReviewService, TypeOrmModule],
 })
 export class AppraisalModule {}
+
--- src/appraisal/quarterlyReview/enums/quarterly-review.enum.ts
+++ src/appraisal/quarterlyReview/enums/quarterly-review.enum.ts
@@ -1,4 +1,10 @@
 export enum ReviewStatus {
   DRAFT = 'Draft',
   SUBMITTED = 'Submitted',
+  IN_REVIEW = 'In Review',
+  REVIEWED = 'Reviewed',
+  APPROVED = 'Approved',
+  COMPLETED = 'Completed',
+  NOT_STARTED = 'Not Started',
 }
+
